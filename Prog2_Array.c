9e9d5d66150ecc65246876fa383d9d57c546c989
shashankagg029 <92911874+shashankagg029@users.noreply.github.com>
2021-10-21 12:46:33 +0530

    Update Prog2_Array.c

--- Prog2_Array.c
+++ Prog2_Array.c
@@ -1,46 +1,45 @@
 #include<stdio.h>
-// removed conio dependency
 
-int showarr(int *arr, int *size)
+intshowarr(int*arr,int*size)
 {
-    for (int i = 0; i < *size; i++)
-    {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
-    return
+for(inti=0;i<*size;i++)
+{
+printf("%d",arr[i]);
+}
+printf("\n");
+return 0;
 }
 
-int addelement(int *arr, int *size, int index, int value)
+intaddelement(int*arr,int*size,intindex,intvalue)
 {
-    for (int i = *size; i > index; i--)
-    {
-        arr[i] = arr[i - 1];
-    }
-    arr[index] = value;
-    *size += 1;
-    return
+for(inti=*size;i>index;i--)
+{
+arr[i]=arr[i-1];
+}
+arr[index]=value;
+*size+=1;
+return 0;
 }
 
-int delelement(int *arr, int *size, int index)
-{   
-    for (int i = index; i < *size - 1; i++)
-    {
-        arr[i] = arr[i + 1];
-    }
-    *size -= 1;
-    return
+intdelelement(int*arr,int*size,intindex)
+{
+for(inti=index;i<*size-1;i++)
+{
+arr[i]=arr[i+1];
+}
+*size-=1;
+return 0;
 }
 
-int main()
+intmain()
 {
-    int size;
-    int arr[20] = {1, 5, 7, 12};
-    size = 4;
-    showarr(arr, &size);
-    addelement(arr, &size, 3, 6);
-    showarr(arr, &size);
-    delelement(arr, &size, 2);
-    showarr(arr, &size);
-    return 0;
+intsize;
+intarr[20]={1,5,7,12};
+size=4;
+showarr(arr,&size);
+addelement(arr,&size,3,6);
+showarr(arr,&size);
+delelement(arr,&size,2);
+showarr(arr,&size);
+return 0;
 }
